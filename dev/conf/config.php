c5b66f225828b1c43fa82f5441130bed43d43d15
Alex Deeley <aedeeley@gmail.com>
2016-05-10 13:27:16 -0400

    HTML site

---
+++ dev/conf/config.php
@@ -0,0 +1,107 @@
+<?php if (!defined('APPLICATION')) exit();
+
+// BasicPages
+$Configuration['BasicPages']['Version'] = '2.1.6';
+
+// Conversations
+$Configuration['Conversations']['Version'] = '2.2';
+
+// Database
+$Configuration['Database']['Name'] = 'adeeleyc_cg2016';
+$Configuration['Database']['Host'] = 'localhost';
+$Configuration['Database']['User'] = 'adeeleyc_cg2016';
+$Configuration['Database']['Password'] = '])37B67PpS';
+
+// EnabledApplications
+$Configuration['EnabledApplications']['Conversations'] = 'conversations';
+$Configuration['EnabledApplications']['Vanilla'] = 'vanilla';
+$Configuration['EnabledApplications']['Yaga'] = 'yaga';
+
+// EnabledPlugins
+$Configuration['EnabledPlugins']['GettingStarted'] = false;
+$Configuration['EnabledPlugins']['HtmLawed'] = 'HtmLawed';
+$Configuration['EnabledPlugins']['NillaBlog'] = true;
+$Configuration['EnabledPlugins']['cleditor'] = false;
+$Configuration['EnabledPlugins']['ButtonBar'] = true;
+$Configuration['EnabledPlugins']['Emotify'] = false;
+$Configuration['EnabledPlugins']['FileUpload'] = false;
+$Configuration['EnabledPlugins']['editor'] = true;
+$Configuration['EnabledPlugins']['EmojiExtender'] = true;
+$Configuration['EnabledPlugins']['Gravatar'] = true;
+$Configuration['EnabledPlugins']['VanillaInThisDiscussion'] = true;
+$Configuration['EnabledPlugins']['OpenID'] = true;
+$Configuration['EnabledPlugins']['AeliaSteamSignIn'] = false;
+$Configuration['EnabledPlugins']['StopForumSpam'] = true;
+$Configuration['EnabledPlugins']['GooglePrettify'] = true;
+$Configuration['EnabledPlugins']['Tagging'] = true;
+$Configuration['EnabledPlugins']['Twitter'] = false;
+$Configuration['EnabledPlugins']['VanillaStats'] = true;
+$Configuration['EnabledPlugins']['vanillicon'] = true;
+$Configuration['EnabledPlugins']['GooglePlus'] = false;
+$Configuration['EnabledPlugins']['Facebook'] = false;
+$Configuration['EnabledPlugins']['SteamSignIn'] = true;
+
+// Garden
+$Configuration['Garden']['Title'] = 'ChilledGamers.com';
+$Configuration['Garden']['Cookie']['Salt'] = 'yqVxKdrYibmYaveP';
+$Configuration['Garden']['Cookie']['Domain'] = '';
+$Configuration['Garden']['Registration']['ConfirmEmail'] = '1';
+$Configuration['Garden']['Registration']['Method'] = 'Captcha';
+$Configuration['Garden']['Registration']['CaptchaPrivateKey'] = '6LeDfhMTAAAAAG-d4UzDdeM98x_BYC2fktErfbgt';
+$Configuration['Garden']['Registration']['CaptchaPublicKey'] = '6LeDfhMTAAAAAJMCdH-3XmL0XcpTocHNbwh5-W9T';
+$Configuration['Garden']['Registration']['InviteExpiration'] = '1 week';
+$Configuration['Garden']['Registration']['InviteRoles']['3'] = '0';
+$Configuration['Garden']['Registration']['InviteRoles']['4'] = '0';
+$Configuration['Garden']['Registration']['InviteRoles']['8'] = '0';
+$Configuration['Garden']['Registration']['InviteRoles']['16'] = '0';
+$Configuration['Garden']['Registration']['InviteRoles']['32'] = '0';
+$Configuration['Garden']['Email']['SupportName'] = 'ChilledGamers.com';
+$Configuration['Garden']['SystemUserID'] = '1';
+$Configuration['Garden']['InputFormatter'] = 'Html';
+$Configuration['Garden']['Version'] = '2.2';
+$Configuration['Garden']['Cdns']['Disable'] = false;
+$Configuration['Garden']['CanProcessImages'] = true;
+$Configuration['Garden']['Installed'] = true;
+$Configuration['Garden']['InstallationID'] = '898B-9155A877-6AA485CF';
+$Configuration['Garden']['InstallationSecret'] = '6db81544a42404237b585076b323144a1f270c95';
+$Configuration['Garden']['Theme'] = 'EmbedFriendly';
+$Configuration['Garden']['HomepageTitle'] = 'ChilledGamers.com';
+$Configuration['Garden']['Description'] = 'A laid back gaming community';
+$Configuration['Garden']['MobileInputFormatter'] = 'TextEx';
+$Configuration['Garden']['AllowFileUploads'] = true;
+
+// Plugins
+$Configuration['Plugins']['GettingStarted']['Dashboard'] = '1';
+$Configuration['Plugins']['GettingStarted']['Plugins'] = '1';
+$Configuration['Plugins']['GettingStarted']['Discussion'] = '1';
+$Configuration['Plugins']['GettingStarted']['Categories'] = '1';
+$Configuration['Plugins']['GettingStarted']['Registration'] = '1';
+$Configuration['Plugins']['GettingStarted']['Profile'] = '1';
+$Configuration['Plugins']['NillaBlog']['PostsPerPage'] = '5';
+$Configuration['Plugins']['NillaBlog']['CategoryIDs'] = false;
+$Configuration['Plugins']['NillaBlog']['DisableCSS'] = false;
+$Configuration['Plugins']['NillaBlog']['GooglePlusOne'] = false;
+$Configuration['Plugins']['editor']['ForceWysiwyg'] = false;
+$Configuration['Plugins']['StopForumSpam']['UserID'] = 3;
+$Configuration['Plugins']['Vanillicon']['Type'] = 'v1';
+$Configuration['Plugins']['OpenID']['DisableSignIn'] = '';
+$Configuration['Plugins']['AeliaSteamSignIn']['Enabled'] = true;
+
+// Routes
+$Configuration['Routes']['DefaultController'] = array('categories', 'Internal');
+$Configuration['Routes']['YWJvdXQoLiop'] = array('page/about$1', 'Internal');
+$Configuration['Routes']['c2VydmVycyguKik='] = array('page/servers$1', 'Internal');
+
+// Vanilla
+$Configuration['Vanilla']['Version'] = '2.2';
+$Configuration['Vanilla']['Discussions']['Layout'] = 'table';
+$Configuration['Vanilla']['Categories']['Layout'] = 'modern';
+$Configuration['Vanilla']['Categories']['Use'] = true;
+$Configuration['Vanilla']['Categories']['MaxDisplayDepth'] = '3';
+$Configuration['Vanilla']['Categories']['DoHeadings'] = false;
+$Configuration['Vanilla']['Categories']['HideModule'] = false;
+
+// Yaga
+$Configuration['Yaga']['Version'] = '1.0.3';
+
+// Last edited by D1RTY (96.252.217.28)2015-12-21 01:18:52
